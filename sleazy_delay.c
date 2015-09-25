4be6312879d62462785c9ddf590fe0879357cad6
Slamkas <seslamka@yahoo.com>
2015-09-24 20:15:15 -0700

    Create sleazy_delay.c

---
+++ sleazy_delay.c
@@ -0,0 +1,18 @@
+//************************************************************************************************************
+// sleazy_delay.c
+//
+// DESCRIPTION
+//
+// AUTHOR
+// Sean E Slamka (sslamka@asu.edu)
+// Computer Science & Engineering
+// Arizona State University, Tempe AZ 85287-8809
+// Web: http://www.devlang.com
+//************************************************************************************************************
+
+#include "sleazy_delay.h"
+
+void sleazy_delay(int p_max_count)
+{
+ for (int i = 0; i < p_max_count; ++i) {}
+}
