fd7e4845b60098ac854a84d7bc3274df169063e5
Slamkas <seslamka@yahoo.com>
2015-09-24 20:14:35 -0700

    Update sim.c

--- sim.c
+++ sim.c
@@ -1,11 +1,34 @@
+//************************************************************************************************************
+// FILE: sim.c
 //
-//  sim.c
+// DESCRIPTION
 //
-//	Proj02
-//  Lab Partners: Sean Slamka
-//  Email: sslamka@asu.edu
-//  CSE325 Embedded Microprocessor Systems
-//  Fall 2015
-//
+// AUTHOR
+// Sean E Slamka (sslamka@asu.edu)
+// Computer Science & Engineering
+// Arizona State University, Tempe AZ 85287-8809
+// Web: http://www.devlang.com
+//************************************************************************************************************
 
 #include "sim.h"
+
+// initilize SIM module
+void sim_hw_init(int port)
+{
+	switch(port)
+	{
+		case 2://Case 2 -> portB SIM running
+			SIM_BASE_PTR->SCGC5 = 1 << SIM_SCGC5_PORTB_SHIFT;
+			break;
+		default:
+			break;
+	}
+}
+
+
+
+
+
+
+
+
