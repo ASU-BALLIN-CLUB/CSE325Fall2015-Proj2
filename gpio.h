081dd15a8559fb295c4004b6a4f5ebc3eea3b8f6
Slamkas <seslamka@yahoo.com>
2015-09-24 20:12:50 -0700

    Update gpio.h

--- gpio.h
+++ gpio.h
@@ -1,9 +1,25 @@
+//************************************************************************************************************
+// FILE: gpio.h
 //
-//  gpio.h
+// DESCRIPTION
 //
-//	Proj02
-//  Lab Partners: Sean Slamka
-//  Email: sslamka@asu.edu
-//  CSE325 Embedded Microprocessor Systems
-//  Fall 2015
-//
+// AUTHOR
+// Sean E Slamka (sslamka@asu.edu)
+// Computer Science & Engineering
+// Arizona State University, Tempe AZ 85287-8809
+// Web: http://www.devlang.com
+//************************************************************************************************************
+
+
+#ifndef GPIO_H_
+#define GPIO_H_
+
+#include "global.h" // Access to int data types and includes mkl46z.h
+#include "sleazy_delay.h" // Brings the declaration of sleazy_delay() into this file.
+
+//forward declair gpio_init function
+void gpio_init(int port, int pin);
+
+
+
+#endif /* GPIO_H_ */
