dd5e06914f8fc9e93fa7e7bf636259846a9247c1
Slamkas <seslamka@yahoo.com>
2015-09-24 20:15:33 -0700

    Create sleazy_delay.h

---
+++ sleazy_delay.h
@@ -0,0 +1,20 @@
+// sleazy_delay.h
+//
+// DESCRIPTION
+//
+// AUTHOR
+// Sean E Slamka (sslamka@asu.edu)
+// Computer Science & Engineering
+// Arizona State University, Tempe AZ 85287-8809
+// Web: http://www.devlang.com
+//************************************************************************************************************
+
+
+#ifndef SLEAZY_DELAY_H_
+#define SLEAZY_DELAY_H_
+
+// Function declaration for sleazy_delay() which is defined in sleazy_delay.c.
+void sleazy_delay(int);
+
+
+#endif /* SLEAZY_DELAY_H_ */
