5c423ac37ea8e5f88d5bf7b41117a512094b80d8
Slamkas <seslamka@yahoo.com>
2015-09-24 20:12:32 -0700

    Update gpio.c

--- gpio.c
+++ gpio.c
@@ -1,12 +1,29 @@
 //
 //  gpio.c
 //
-//	Proj02
-//  Lab Partners: Sean Slamka
-//  Email: sslamka@asu.edu
-//  CSE325 Embedded Microprocessor Systems
-//  Fall 2015
+// DESCRIPTION
 //
-
+// AUTHOR
+// Sean E Slamka (sslamka@asu.edu)
+// Computer Science & Engineering
+// Arizona State University, Tempe AZ 85287-8809
+// Web: http://www.devlang.com
+//************************************************************************************************************
 
 #include "gpio.h"
+
+
+//Initilze GPIO port and pin.
+void gpio_init(int port, int pin)
+{
+	switch (port)
+	{
+		case 2:// CASE 2 => PORTB
+			PORTB_PCR(pin) |= 0b001 << 8;
+			GPIOB_PDDR |= 1 << pin;
+			break;
+		default:
+			break;
+	}
+
+}
