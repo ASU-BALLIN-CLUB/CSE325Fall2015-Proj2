dc05ec553148b2e51559221050724189f94724b0
Slamkas <seslamka@yahoo.com>
2015-09-24 20:13:21 -0700

    Update led.h

--- led.h
+++ led.h
@@ -1,9 +1,28 @@
+//************************************************************************************************************
+// FILE: led.h
 //
-//  led.h
+// DESCRIPTION
 //
-//	Proj02
-//  Lab Partners: Sean Slamka
-//  Email: sslamka@asu.edu
-//  CSE325 Embedded Microprocessor Systems
-//  Fall 2015
-//
+// AUTHOR
+// Sean E Slamka (sslamka@asu.edu)
+// Computer Science & Engineering
+// Arizona State University, Tempe AZ 85287-8809
+// Web: http://www.devlang.com
+//************************************************************************************************************
+
+
+#ifndef LED_H_
+#define LED_H_
+
+#include "global.h" // Access to int data types and includes mkl46z.h
+#include "sleazy_delay.h" // Brings the declaration of sleazy_delay() into this file.
+
+
+//forward declaration functions
+void led_on(int pin);
+void led_off(int pin);
+void led_all_off();
+void led_all_on();
+
+
+#endif /* LED_H_ */
