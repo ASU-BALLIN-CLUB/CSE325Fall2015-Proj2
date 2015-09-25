aefc451738363fdeed428458cba6f94648c54d3c
Slamkas <seslamka@yahoo.com>
2015-09-24 20:14:52 -0700

    Update sim.h

--- sim.h
+++ sim.h
@@ -1,9 +1,34 @@
 //
 //  sim.h
 //
-//	Proj02
-//  Lab Partners: Sean Slamka
-//  Email: sslamka@asu.edu
-//  CSE325 Embedded Microprocessor Systems
-//  Fall 2015
+// DESCRIPTION
 //
+// AUTHOR
+// Sean E Slamka (sslamka@asu.edu)
+// Computer Science & Engineering
+// Arizona State University, Tempe AZ 85287-8809
+// Web: http://www.devlang.com
+//************************************************************************************************************
+//
+//  gpio.c
+//
+//	Proj03
+//  Lab Partners: Sean Slamka, Aydin Balci
+//  Email: sslamka@asu.edu, abalci@asu.edu
+//  CSE325 Embedded Microprocessor Systems
+//  Fall 2014
+//
+
+#ifndef SIM_H_
+#define SIM_H_
+
+#include "global.h" // Access to int data types and includes mkl46z.h
+#include "sleazy_delay.h" // Brings the declaration of sleazy_delay() into this file.
+
+
+//forward declaration functions
+void sim_hw_init(int port);
+
+
+
+#endif /* SIM_H_ */
